8a518fab86b78189843d1e18b88476cf4798cbc6
Qianfugen <287541326@qq.com>
2020-01-19 23:45:56 +0800

    rocketmq消息队列发送消息

---
+++ src/main/java/BroadcastConsumer.java
@@ -0,0 +1,34 @@
+import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
+import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
+import org.apache.rocketmq.common.message.MessageExt;
+import org.apache.rocketmq.common.protocol.heartbeat.MessageModel;
+
+import java.util.List;
+
+public class BroadcastConsumer {
+    public static void main(String[] args) throws Exception {
+        // 实例化消息生产者,指定组名
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("group1");
+        // 指定Namesrv地址信息.
+        consumer.setNamesrvAddr("localhost:9876");
+        // 订阅Topic
+        consumer.subscribe("Test", "*");
+        //广播模式消费
+        consumer.setMessageModel(MessageModel.BROADCASTING);
+        // 注册回调函数，处理消息
+        consumer.registerMessageListener(new MessageListenerConcurrently() {
+            @Override
+            public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> msgs,
+                                                            ConsumeConcurrentlyContext context) {
+                System.out.printf("%s Receive New Messages: %s %n",
+                        Thread.currentThread().getName(), msgs);
+                return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+            }
+        });
+        //启动消息者
+        consumer.start();
+        System.out.printf("Consumer Started.%n");
+    }
+}
